d1da1d6b6fd9b3bfe6d74a4d07edb7efdcff7eea
Justin Quinn <justinquinn51691@gmail.com>
2024-10-03 07:58:38 -0400

    reverting

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img id="portrait" alt="Portrait" src="./assets/portrait.jpg" />
-  <MightyQuinn msg="Welcome to The Mighty Quinn Music (updating site name)"/>
+  <MightyQuinn msg="Welcome to The Mighty Quinn Music"/>
 </template>
 
 <script>
